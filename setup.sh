efc4d2aaaf7250fd471968d60e6b451d9f469eed
Tony Wildish <Tony.Wildish@cern.ch>
2014-03-03 12:40:37 +0100

    generalise...

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 unset PERL5LIB
-. /data/current/sw*/slc*/cms/PHEDEX-micro/*/etc/profile.d/init.sh
-. /data/current/sw*/slc*/cms/PHEDEX-lifecycle/*/etc/profile.d/init.sh
-. /data/current/sw*/slc*/cms/asyncstageout/*/etc/profile.d/init.sh
+root=/data/current
+. $root/sw*/slc*/cms/PHEDEX-micro/*/etc/profile.d/init.sh
+. $root/sw*/slc*/cms/PHEDEX-lifecycle/*/etc/profile.d/init.sh
+. $root/sw*/slc*/cms/asyncstageout/*/etc/profile.d/init.sh
 
 # Remove old FakeFTS.pl from LifeCycle distribution!
-(rm /data/current/sw*/slc*/cms/PHEDEX-lifecycle/*/bin/FakeFTS.pl) 2>/dev/null
+(rm $root/sw*/slc*/cms/PHEDEX-lifecycle/*/bin/FakeFTS.pl) 2>/dev/null
 
 # Add PHEDEX-micro Utilities to the PATH
-export PATH=${PATH}:`ls -d /data/current/sw*/slc*/cms/PHEDEX-micro/*/Utilities`
+export PATH=${PATH}:`ls -d $root/sw*/slc*/cms/PHEDEX-micro/*/Utilities`
 
-export PERL5LIB=${PERL5LIB}:/data/wildish/monitor/perl_lib
+export PERL5LIB=${PERL5LIB}:`pwd`/perl_lib
